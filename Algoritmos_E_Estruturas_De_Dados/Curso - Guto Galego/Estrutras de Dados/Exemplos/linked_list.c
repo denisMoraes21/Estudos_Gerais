9a51f65b6dfd50de6e0c38e9a09cef937655920b
denis.guimaraes <denis.guimaraes@fpf.br>
2026-06-17 10:31:29 -0400

    linked list

--- Algoritmos_E_Estruturas_De_Dados/Curso - Guto Galego/Estrutras de Dados/Exemplos/linked_list.c
+++ Algoritmos_E_Estruturas_De_Dados/Curso - Guto Galego/Estrutras de Dados/Exemplos/linked_list.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// (head) value 13 : address h -> x <- List->first
+// (node) value 36 : address x -> y
+// (node) value 39 : address y -> z
+// (node) value 52 : address z -> t
+// (tail) value 65 : address t -> NULL
+
 typedef struct Node {
     int value;
     struct Node *next;
 } Node;
 
-Node* insert_begin(Node *head, int value) {
-    Node *new_node = malloc(sizeof(Node));
-    if (new_node == NULL) {
-        return head;
-    }
+typedef struct LinkedList {
+    int size;
+    Node *first;
+} List;
 
-    new_node->value = value;
-    new_node->next = head;
-
-    return new_node;
+List *createList(List *p_list) {
+    p_list = (List *) malloc(sizeof(List));
+    p_list->size = 0;
+    p_list->first = NULL;
+    return p_list; 
 }
 
-void print_list(Node *head) {
-    Node *current = head;
+int emptyList(List *p_list) {
+    return p_list->first == NULL;
+}
+
+void insertBegin(List *p_list, int value) {
+    Node *p_new_node = (Node *) malloc(sizeof(Node));
+    p_new_node->value = value;          // p_new_node : value [13] : address a -> NULL
+    p_new_node->next = p_list->first;   // p_new_node : value [13] : address a -> h (value [36] : address h)
+    p_list->first = p_new_node;         // p_list->first : value [13] : address h -> x (value [36] : address x)
+    p_list->size = p_list->size + 1;
+}
+
+void printListPre(List *p_list) {
+    /* @brief prefixed print list */
+    Node *current = p_list->first;
+    printf("Lista: ");
+
     while (current != NULL) {
         printf("%d -> ", current->value);
         current = current->next;
@@ -28,25 +49,59 @@ void print_list(Node *head) {
     printf("NULL\n");
 }
 
-void free_list(Node *head) {
-    Node *current = head;
+void freeList(List *p_list) {
+    Node *current = p_list->first;
 
     while (current != NULL) {
-        Node *temp = current;
-        current = current->next;
-        free(temp);
+        Node *temp = current->next;
+        free(current);
+        current = temp;
     }
+    free(p_list);
+}
+
+List *removeItem(List *p_list, int value) {
+    Node *p_current_node = p_list->first;
+    Node *p_last_node = NULL;
+    while (p_current_node != NULL && p_current_node->value != value) {
+        p_last_node = p_current_node;
+        p_current_node = p_current_node->next;
+    }
+    if (p_current_node == NULL) return p_list;
+    if (p_current_node == p_list->first) p_list->first = p_list->first->next;
+    else p_last_node->next = p_current_node->next;
+    free(p_current_node);
+    printf("Valor %d removido da list\n", value);
+    p_list->size = p_list->size - 1;
+    return p_list;
+}
+
+Node *search(List *p_list, int value) {
+    Node *p_current_node = p_list->first;
+    while (p_current_node != NULL) {
+        if (p_current_node->value == value) return p_current_node;
+        p_current_node = p_current_node->next;
+    }
+    return NULL;
 }
 
 int main() {
-    Node *head = NULL;
+    List *linked_list;
+    linked_list = createList(linked_list);
+    insertBegin(linked_list, 65);
+    insertBegin(linked_list, 52);
+    removeItem(linked_list, 65);
+    insertBegin(linked_list, 39);
+    insertBegin(linked_list, 36);
+    insertBegin(linked_list, 13);
+    printListPre(linked_list);
 
-    head = insert_begin(head, 30);
-    head = insert_begin(head, 20);
-    head = insert_begin(head, 10);
-
-    print_list(head);
-
-    free_list(head);
+    Node *node;
+    int value = 39;
+    node = search(linked_list, value);
+    if (node != NULL) printf("Valor %d encontrado\n", value); 
+    else printf("Valor %d nao encontrado\n", value);
+    
+    freeList(linked_list);
     return 0;
 }
