ea0adb38ef2123b0d8606d3a41644ec6d14046da
denis.guimaraes <denis.guimaraes@fpf.br>
2026-07-09 09:49:43 -0400

    Update: revisão de conceitos de sistemas de build

---
+++ Languages/C++/CMAKE-NINJA/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "soma.h"
+
+int main() {
+    int resultado = soma(10, 20);
+
+    std::cout << "Resultado = " << resultado << std::endl;
+
+    return 0;
+}
---
+++ Languages/C++/ReadMe.md
@@ -0,0 +1,9 @@
+### Unix Makefiles
+
+- cmake -G "Unix Makefiles" -B build
+- make -C build
+
+### Ninja
+
+* cmake -G Ninja -B build
+* ninja -C build
