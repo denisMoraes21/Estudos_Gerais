187e3497ea0c0ec42e59b1a42c688b5d85a8ce10
denis.guimaraes <denis.guimaraes@fpf.br>
2026-07-24 09:41:45 -0400

    update

--- Microcontrolers/stm/test/CM4/Src/ethernetif.c
+++ Microcontrolers/stm/test/CM4/Src/ethernetif.c
@@ -139,6 +139,7 @@ osSemaphoreId_t TxPktSemaphore = NULL;   /* Semaphore to signal transmit packet 
 
 /* Global Ethernet handle */
 ETH_HandleTypeDef heth;
+ETH_TxPacketConfig TxConfig;
 
 /* Private function prototypes -----------------------------------------------*/
 int32_t ETH_PHY_IO_Init(void);
@@ -237,10 +238,10 @@ static void low_level_init(struct netif *netif)
 
   hal_eth_init_status = HAL_ETH_Init(&heth);
 
-  memset(&TxConfig, 0 , sizeof(ETH_TxPacketConfig));
-  TxConfig.Attributes = ETH_TX_PACKETS_FEATURES_CSUM | ETH_TX_PACKETS_FEATURES_CRCPAD;
-  TxConfig.ChecksumCtrl = ETH_CHECKSUM_IPHDR_PAYLOAD_INSERT_PHDR_CALC;
-  TxConfig.CRCPadCtrl = ETH_CRC_PAD_INSERT;
+  // memset(&TxConfig, 0 , sizeof(ETH_TxPacketConfig));
+  // TxConfig.Attributes = ETH_TX_PACKETS_FEATURES_CSUM | ETH_TX_PACKETS_FEATURES_CRCPAD;
+  // TxConfig.ChecksumCtrl = ETH_CHECKSUM_IPHDR_PAYLOAD_INSERT_PHDR_CALC;
+  // TxConfig.CRCPadCtrl = ETH_CRC_PAD_INSERT;
 
   /* End ETH HAL Init */
 
--- Microcontrolers/stm/test/CM4/Src/main.c
+++ Microcontrolers/stm/test/CM4/Src/main.c
@@ -86,8 +86,6 @@ int main(void)
 #if defined(DUAL_CORE_BOOT_SYNC_SEQUENCE)
   __HAL_RCC_HSEM_CLK_ENABLE();
   HAL_HSEM_ActivateNotification(__HAL_HSEM_SEMID_TO_MASK(HSEM_ID_0));
-  Domain D2 goes to STOP mode (Cortex-M4 in deep-sleep) waiting for Cortex-M7 to
-  perform system initialization (system clock config, external memory configuration.. )
   HAL_PWREx_ClearPendingEvent();
   HAL_PWREx_EnterSTOPMode(PWR_MAINREGULATOR_ON, PWR_STOPENTRY_WFE, PWR_D2_DOMAIN);
   __HAL_HSEM_CLEAR_FLAG(__HAL_HSEM_SEMID_TO_MASK(HSEM_ID_0));
